a6c3cfa0de9ab56dbe78789797d0c4195b67cfe9
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-01 11:22:17 -0400

    add body parser usage

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+const bp = require('body-parser')
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(bp.json())
+
+
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
