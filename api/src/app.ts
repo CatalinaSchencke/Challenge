67b0db72b5f010a8dcaeedf94a9018f001f87a17
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-01 11:29:13 -0400

    add POST request for add user
    Define an email for id
    add one service as an element of array

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,32 @@
 import express from 'express';
 const bp = require('body-parser')
 import {db} from './firebase';
-
+import { doc, collection, setDoc, arrayUnion, updateDoc } from "firebase/firestore"; 
 
 const app = express();
 const port = 3000;
 
 app.use(bp.json())
+
+app.post('/addUser', async (req, res) => {
+  // Añadir un usuario a la base de datos (con el correo como id)
+  const id = req.body.email;
+  const userdb = collection(db, "Users");
+  const userRef = doc(userdb, id);
+
+  // Añadir un servicio para una creación de usuario correcta
+  const userJson ={
+    date: req.body.date,
+    service: req.body.service,
+    value: req.body.value,
+    pending: req.body.pending
+  }
+  
+  const response = setDoc(userRef, {
+    services: arrayUnion(userJson)
+  });
+  res.send(response);
+});
 
 
 
