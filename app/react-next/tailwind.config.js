60621fd26d2caba3ef90f88d7207fcc65b15c9cc
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-02 03:10:30 -0400

    add color palete to tailwind config file

--- app/react-next/tailwind.config.js
+++ app/react-next/tailwind.config.js
@@ -7,6 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#FFAE03',
+          50: '#FFE9BB',
+          100: '#FFE2A6',
+          200: '#FFD57D',
+          300: '#FFC855',
+          400: '#FFBB2C',
+          500: '#FFAE03',
+          600: '#CA8900',
+          700: '#926300',
+          800: '#5A3D00',
+          900: '#221700',
+          950: '#060400'
+        },
+        secondary: '#FFECCB',
+        tertiary: '#FDF4E5',
+        darkBrown: '#A58356'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
