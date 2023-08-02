fac566d5eb73b5c1c0848340b930890e0393f7aa
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-02 03:24:29 -0400

    fix nav component import being wrong

--- app/react-next/src/app/layout.tsx
+++ app/react-next/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 
-import Nav from './components/Nav'
+import Nav from './components/nav'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
