78a4d3d8ecb894b661ee75d9fc5680c5b3d7ca63
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-02 03:22:12 -0400

    add login to new Home component

--- app/react-next/src/app/page.tsx
+++ app/react-next/src/app/page.tsx
@@ -1,7 +1,34 @@
-import Image from 'next/image'
+
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+
+import LogIn from './authentication/log-in'
 
 export default function Home() {
+
+  const router = useRouter();
+  const loggedIn = false;
+  const email = "example@example.com";
+
   return (
-    <div></div>
+    <>
+      {!loggedIn
+        ?
+        <LogIn />
+        :
+        <>
+          <div className="flex h-[calc(100vh_-_5rem)] bg-tertiary justify-center items-center">
+            <div className="flex flex-col items-center gap-4">
+              <h2 className='text-3xl font-medium'>Iniciaste sesión como <span className="text-primary font-semibold">{email}</span></h2>
+              <Link href="/dashboard" className="px-20 py-2 rounded-xl bg-primary text-white font-medium">Ir al dashboard</Link>
+            </div>
+          </div>
+        </>
+
+      }
+    </>
   )
 }
